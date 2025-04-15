67b80cb5c2405ac8d8e9a736448c8ae0551e7985
ThuyTien001 <152962262+ThuyTien001@users.noreply.github.com>
2025-04-15 16:29:25 +0700

    fix

--- config/index.js
+++ config/index.js
@@ -1,76 +1,50 @@
-// // db.js
-// const mysql = require('mysql2');
-// require('dotenv').config();
-
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
-//   timezone: 'Z',
-// }).promise();
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL:', err.message);
-//     return;
-//   }
-//   console.log('Connected to MySQL database');
-// });
-
-// module.exports = connection;
-
-// // require('dotenv').config();
-// // const mysql = require('mysql2/promise');
-// // const url = require('url');
-
-// // const dbUrl = process.env.JAWSDB_URL;
-// // if (!dbUrl) {
-// //     console.error('JAWSDB_URL không tồn tại!');
-// //     process.exit(1);
-// // }
-
-// // const params = url.parse(dbUrl);
-// // const [user, password] = params.auth.split(':');
-// // const host = params.hostname;
-// // const database = params.pathname.replace('/', '');
-
-// // const pool = mysql.createPool({
-// //     host,
-// //     user,
-// //     password,
-// //     database,
-// //     waitForConnections: true,
-// //     connectionLimit: 10,
-// //     queueLimit: 0,
-// // });
-
-// // module.exports = pool;
-
-
 // db.js
-const { Pool } = require('pg');
+const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = new Pool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5433,
-  ssl: {
-    rejectUnauthorized: false, // cần thiết khi kết nối với Yugabyte Cloud
-  }
-});
+  port: process.env.DB_PORT,
+  timezone: 'Z',
+}).promise();
 
-pool.connect((err, client, release) => {
+connection.connect((err) => {
   if (err) {
-    console.error('❌ Error connecting to YugabyteDB:', err.stack);
-  } else {
-    console.log('✅ Connected to YugabyteDB');
-    release();
+    console.error('Error connecting to MySQL:', err.message);
+    return;
   }
+  console.log('Connected to MySQL database');
 });
 
-module.exports = pool;
+module.exports = connection;
+
+// require('dotenv').config();
+// const mysql = require('mysql2/promise');
+// const url = require('url');
+
+// const dbUrl = process.env.JAWSDB_URL;
+// if (!dbUrl) {
+//     console.error('JAWSDB_URL không tồn tại!');
+//     process.exit(1);
+// }
+
+// const params = url.parse(dbUrl);
+// const [user, password] = params.auth.split(':');
+// const host = params.hostname;
+// const database = params.pathname.replace('/', '');
+
+// const pool = mysql.createPool({
+//     host,
+//     user,
+//     password,
+//     database,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+// });
+
+// module.exports = pool;
+
